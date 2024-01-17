c58dd4ca8c88e1a996eea3cc46e1b05baf994720
Mina <146908931+mina-ghanadi@users.noreply.github.com>
2024-01-17 13:57:48 +0330

    add highlight part in main page

---
+++ app.js
@@ -0,0 +1,57 @@
+function readMore1(){
+    let tag1=document.getElementById("tag1");
+    let tE=document.getElementById("tag1-expand-button");
+    let tL=document.getElementById("tag1-less-button");
+    if (tag1.style.display === "none") {
+        tag1.style.display = "block";
+        tE.style.display="none";
+        tL.style.display="inline";
+      } else {
+        tag1.style.display = "none";
+        tE.style.display="inline";
+        tL.style.display="none";
+
+      }
+    }
+    function readMore2(){
+        let tag1=document.getElementById("tag2");
+        let tE=document.getElementById("tag2-expand-button");
+        let tL=document.getElementById("tag2-less-button");
+        if (tag1.style.display === "none") {
+            tag1.style.display = "block";
+            tE.style.display="none";
+            tL.style.display="inline";
+          } else {
+            tag1.style.display = "none";
+            tE.style.display="inline";
+            tL.style.display="none";
+          }
+        }
+        function readMore3(){
+            let tag1=document.getElementById("tag3");
+            let tE=document.getElementById("tag3-expand-button");
+            let tL=document.getElementById("tag3-less-button");
+            if (tag1.style.display === "none") {
+                tag1.style.display = "block";
+                tE.style.display="none";
+               tL.style.display="inline";
+              } else {
+                tag1.style.display = "none";
+                tE.style.display="inline";
+                tL.style.display="none";
+              }
+            }
+            function readMore4(){
+                let tag1=document.getElementById("tag4");
+                let tE=document.getElementById("tag4-expand-button");
+            let tL=document.getElementById("tag4-less-button");
+                if (tag1.style.display === "none") {
+                    tag1.style.display = "block";
+                    tE.style.display="none";
+                    tL.style.display="inline";
+                  } else {
+                    tag1.style.display = "none";
+                    tE.style.display="inline";
+                    tL.style.display="none";
+                  }
+                }
